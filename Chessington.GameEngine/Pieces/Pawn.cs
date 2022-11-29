422efd3ea31ae6eb238067fc6e5e9052bb8a81ec
Lucy Kelsall <lucy.kelsall@softwire.com>
2022-11-29 17:19:44 +0000

    White pawns can move one square up and black pawns one square down

--- Chessington.GameEngine/Pieces/Pawn.cs
+++ Chessington.GameEngine/Pieces/Pawn.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Security.Policy;
 
 namespace Chessington.GameEngine.Pieces
 {
@@ -10,7 +11,21 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            List<Square> pawnMovesList = new List<Square>();
+            var pawnLocation = board.FindPiece(this);
+            
+            if (this.Player == Player.White)
+            {
+                var availableSquare = Square.At(pawnLocation.Row - 1, pawnLocation.Col);
+                pawnMovesList.Add(availableSquare);
+            }
+            else
+            {
+                var availableSquare = Square.At(pawnLocation.Row + 1, pawnLocation.Col);
+                pawnMovesList.Add(availableSquare);
+            }
+
+            return pawnMovesList;
         }
     }
 }
