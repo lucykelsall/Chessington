3b23abfc9260864c8a3b02fe858bc65fe173cd36
Lucy Kelsall <lucy.kelsall@softwire.com>
2022-11-30 14:08:58 +0000

    Rooks can move laterally

--- Chessington.GameEngine/Pieces/Rook.cs
+++ Chessington.GameEngine/Pieces/Rook.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using Chessington.GameEngine.Pieces;
+using Chessington.GameEngine;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Chessington.GameEngine.Pieces
@@ -10,7 +12,18 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            var rookMovesList = new List<Square>();
+            var rookLocation = board.FindPiece(this);
+
+            for (var i = 0; i < 8; i++)
+            {
+                rookMovesList.Add(Square.At(rookLocation.Row, i));
+                rookMovesList.Add(Square.At(i, rookLocation.Col));
+            }
+
+            rookMovesList.RemoveAll(item => item == rookLocation);
+
+            return rookMovesList;
         }
     }
 }
