4550a2f24a269ae2ac18f5ff13ff0d3d4f8d0891
Lucy Kelsall <lucy.kelsall@softwire.com>
2022-12-02 13:40:02 +0000

    Bishops cannot pass through friendly or opposing pieces

--- Chessington.GameEngine/Pieces/Bishop.cs
+++ Chessington.GameEngine/Pieces/Bishop.cs
@@ -14,26 +14,93 @@ namespace Chessington.GameEngine.Pieces
         {
             var bishopMovesList = new List<Square>();
             var bishopLocation = board.FindPiece(this);
-
-            for (var i = 0; i < 8; i++)
+            
+            for (var i = 1; i < 8; i++)
             {
-                var bishopMoves = new List<Square> { 
-                    Square.At(bishopLocation.Row + i, bishopLocation.Col + i), 
-                    Square.At(bishopLocation.Row + i, bishopLocation.Col - i),
-                    Square.At(bishopLocation.Row - i, bishopLocation.Col + i),
-                    Square.At(bishopLocation.Row - i, bishopLocation.Col - i)
-                };
+                var squareToMoveTo = Square.At(bishopLocation.Row + i, bishopLocation.Col + i);
 
-                foreach (var move in bishopMoves)
+                if (squareToMoveTo.isSquareOnBoard() == true)
                 {
-                    if (move.isSquareOnBoard() == true)
+                    if (board.GetPiece(squareToMoveTo) == null && squareToMoveTo.isSquareOnBoard() == true)
                     {
-                        bishopMovesList.Add(move);
+                        bishopMovesList.Add(squareToMoveTo);
+                    }
+                    else if (board.GetPiece(squareToMoveTo).Player != this.Player &&
+                             squareToMoveTo.isSquareOnBoard() == true)
+                    {
+                        bishopMovesList.Add(squareToMoveTo);
+                        break;
+                    }
+                    else if (board.GetPiece(squareToMoveTo).Player == this.Player &&
+                             squareToMoveTo.isSquareOnBoard() == true)
+                    {
+                        break;
+                    }
+                }
+
+                var otherSquareToMoveTo = Square.At(bishopLocation.Row + i, bishopLocation.Col - i);
+
+                if (otherSquareToMoveTo.isSquareOnBoard() == true)
+                {
+                    if (board.GetPiece(otherSquareToMoveTo) == null && otherSquareToMoveTo.isSquareOnBoard() == true)
+                    {
+                        bishopMovesList.Add(otherSquareToMoveTo);
+                    }
+                    else if (board.GetPiece(otherSquareToMoveTo).Player != this.Player &&
+                             otherSquareToMoveTo.isSquareOnBoard() == true)
+                    {
+                        bishopMovesList.Add(otherSquareToMoveTo);
+                        break;
+                    }
+                    else if (board.GetPiece(otherSquareToMoveTo).Player == this.Player &&
+                             otherSquareToMoveTo.isSquareOnBoard() == true)
+                    {
+                        break;
+                    }
+                }
+
+                var xSquareToMoveTo = Square.At(bishopLocation.Row - i, bishopLocation.Col + i);
+
+                if (xSquareToMoveTo.isSquareOnBoard() == true)
+                {
+                    if (board.GetPiece(xSquareToMoveTo) == null && xSquareToMoveTo.isSquareOnBoard() == true)
+                    {
+                        bishopMovesList.Add(xSquareToMoveTo);
+                    }
+                    else if (board.GetPiece(xSquareToMoveTo).Player != this.Player &&
+                             xSquareToMoveTo.isSquareOnBoard() == true)
+                    {
+                        bishopMovesList.Add(xSquareToMoveTo);
+                        break;
+                    }
+                    else if (board.GetPiece(xSquareToMoveTo).Player == this.Player &&
+                             xSquareToMoveTo.isSquareOnBoard() == true)
+                    {
+                        break;
+                    }
+                }
+
+                var ySquareToMoveTo = Square.At(bishopLocation.Row - i, bishopLocation.Col - i);
+
+                if (ySquareToMoveTo.isSquareOnBoard() == true)
+                {
+                    if (board.GetPiece(ySquareToMoveTo) == null && ySquareToMoveTo.isSquareOnBoard() == true)
+                    {
+                        bishopMovesList.Add(ySquareToMoveTo);
+                    }
+                    else if (board.GetPiece(ySquareToMoveTo).Player != this.Player &&
+                             ySquareToMoveTo.isSquareOnBoard() == true)
+                    {
+                        bishopMovesList.Add(ySquareToMoveTo);
+                        break;
+                    }
+                    else if (board.GetPiece(ySquareToMoveTo).Player == this.Player &&
+                             ySquareToMoveTo.isSquareOnBoard() == true)
+                    {
+                        break;
                     }
                 }
             }
-
-            bishopMovesList.RemoveAll(item => item == bishopLocation);
 
             return bishopMovesList;
         }
